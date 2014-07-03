19eaf91a304ad39dd1f5480c39b295df05417f71
hyasser <heidi.yasser@telepin.com>
2014-07-03 14:42:49 +0200

    Update LocationInfo.js

--- www/LocationInfo.js
+++ www/LocationInfo.js
@@ -1,37 +1,48 @@
-var exec = require("cordova/exec"), channel = require('cordova/channel'), argscheck = require('cordova/argscheck'), utils = require('cordova/utils');
+var exec = require("cordova/exec"), channel = require('cordova/channel'), argscheck = require('cordova/argscheck'), utils = require('cordova/utils'), cordova = require('cordova');
 
-channel.createSticky('OnCordovaConnectionReady');
-// Tell cordova channel to wait on the CordovaInfoReady event
-channel.waitForInitialization('OnCordovaConnectionReady');
+//channel.createSticky('OnCordovaConnectionReady');
+//// Tell cordova channel to wait on the CordovaInfoReady event
+//channel.waitForInitialization('OnCordovaConnectionReady');
 
-// var LocationInfo = function() {
-function LocationInfo() {
+channel.createSticky('onCordovaInfoReady');
+//Tell cordova channel to wait on the CordovaInfoReady event
+channel.waitForInitialization('onCordovaInfoReady');
+
+
+// function locationInfo() {
+// this.cid = null;
+// alert("inside location info");
+// }
+// var me = new locationInfo();
+
+//var locationInfo = function() {
+function locationInfo() {
 	this.cid = null;
-	alert("inside location info");
-}
 
-var me = new LocationInfo();
-
-channel.onCordovaReady.subscribe(function() {
-	alert("READY!!");
-	me.getCID(function(info) {
-		alert("getInfo");
-		me.cid = info.cid;
-		alert(me.cid);
-		alert(info.cid);
-		channel.OnCordovaConnectionReady.fire();
-	}, function(e) {
-		utils.alert("[ERROR] Error initializing Cordova: " + e);
+	channel.onCordovaReady.subscribe(function() {
+		alert("READY!!");
+		this.getInfo(function(info) {
+			alert("getInfo");
+//			this.cid = info.cid;
+//			alert(this.cid);
+//			alert(info.cid);
+			//channel.OnCordovaConnectionReady.fire();
+			 channel.onCordovaInfoReady.fire();
+		}, function(e) {
+			utils.alert("[ERROR] Error initializing Cordova: " + e);
+		});
 	});
-});
-
-// };
+}
+//};
 //
-LocationInfo.prototype.getCID = function(successCallback, errorCallback) {
-	argscheck.checkArgs('fF', 'LocationInfo.getCID', arguments);
-	cordova.exec(successCallback, errorCallback, 'LocationInfo', 'sendCID', []);
+
+
+locationInfo.prototype.getInfo = function(successCallback, errorCallback) {
+	alert("prototype");
+	argscheck.checkArgs('fF', 'locationInfo.getInfo', arguments);
+	exec(successCallback, errorCallback, 'LocationInfo', 'getCID', []);
 }
 
-module.exports = me;
-// var locationInfo = new LocationInfo();
-// module.exports = locationInfo;
+//module.exports = me;
+// var locationInfo = new locationInfo();
+ module.exports = new locationInfo();
