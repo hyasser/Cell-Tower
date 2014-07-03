03ef67a58df759680f9cbce0eebd19d8c4ac808d
hyasser <heidi.yasser@telepin.com>
2014-07-03 13:33:34 +0200

    Update LocationInfo.js

--- www/LocationInfo.js
+++ www/LocationInfo.js
@@ -1,22 +1,37 @@
-var exec = require("cordova/exec");
+var exec = require("cordova/exec"), channel = require('cordova/channel'), argscheck = require('cordova/argscheck'), utils = require('cordova/utils');
 
-var LocationInfo = function() {
-	alert("location info");
-};
-//
-LocationInfo.prototype.getCID = function(successCallback, errorCallback) {
-	cordova.exec(successCallback,
-	// success callback function
-	errorCallback,
-	// error callback function
-	'LocationInfo',
-	// name of the native java class
-	'locationAction',
-	// name of the action to performed
-	[]
-	// and this array of custom arguments to create our entry
-	);
+channel.createSticky('OnCordovaConnectionReady');
+// Tell cordova channel to wait on the CordovaInfoReady event
+channel.waitForInitialization('OnCordovaConnectionReady');
+
+// var LocationInfo = function() {
+function LocationInfo() {
+	this.cid = null;
+	alert("inside location info");
 }
 
-var locationInfo = new LocationInfo();
-module.exports = locationInfo;
+var me = new LocationInfo();
+
+channel.onCordovaReady.subscribe(function() {
+	alert("READY!!");
+	me.getCID(function(info) {
+		alert("getInfo");
+		me.cid = info.cid;
+		alert(me.cid);
+		alert(info.cid);
+		channel.OnCordovaConnectionReady.fire();
+	}, function(e) {
+		utils.alert("[ERROR] Error initializing Cordova: " + e);
+	});
+});
+
+// };
+//
+LocationInfo.prototype.getCID = function(successCallback, errorCallback) {
+	argscheck.checkArgs('fF', 'LocationInfo.getCID', arguments);
+	cordova.exec(successCallback, errorCallback, 'LocationInfo', 'sendCID', []);
+}
+
+module.exports = me;
+// var locationInfo = new LocationInfo();
+// module.exports = locationInfo;
